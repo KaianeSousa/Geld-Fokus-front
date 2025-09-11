9cb0fe0a2918ef499c9d1faf6130c969de4f5734
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-10 22:47:04 -0300

    feat: Add StocksSidebar component to main app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Navbar} from './components/navbar/navbar';
-import {Footer} from './components/footer/footer';
+import {Navbar} from './shared/components/navbar/navbar';
+import {Footer} from './shared/components/footer/footer';
+import {StocksSidebar} from './shared/components/stocks-sidebar/stocks-sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+   imports: [RouterOutlet, Navbar, Footer, StocksSidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
