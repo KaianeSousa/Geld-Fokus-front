d345a5074a9403450048078f44e8d9542d0833bc
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-18 00:38:58 -0300

    Refactor: Update stock sidebar to use async pipe and display loading/error states

--- src/app/shared/components/stocks-sidebar/stocks-sidebar.ts
+++ src/app/shared/components/stocks-sidebar/stocks-sidebar.ts
@@ -2,31 +2,51 @@ import { Component, inject, OnInit } from '@angular/core';
 import { StockWidgetComponent } from "../stock-widget/stock-widget";
 import { Stock } from '../../../core/@types/Stock';
 import { StockService } from '../../../core/services/stock.service';
+import {map, Observable, of, startWith} from 'rxjs';
+import {Article} from '../../../core/@types/Article';
+import {catchError} from 'rxjs/operators';
+import {AsyncPipe} from '@angular/common';
+
+interface StocksState {
+   loading: boolean;
+   stocks: Stock[] | null;
+   error: Error | null;
+}
 
 @Component({
   selector: 'app-stocks-sidebar',
-  imports: [StockWidgetComponent],
+   imports: [StockWidgetComponent, AsyncPipe],
   templateUrl: './stocks-sidebar.html',
   styleUrl: './stocks-sidebar.scss'
 })
 export class StocksSidebar implements OnInit {
-  acoes: Stock[] = [];
-  acoesBaratas: Stock[] = [];
-  acoesDestaque: Stock[] = [];
+   expensiveStocksState$!: Observable<StocksState>;
+   cheapStocksState$!: Observable<StocksState>;
+   featuredStocksState$!: Observable<StocksState>;
 
-  private stockService = inject(StockService);
+   private stockService = inject(StockService);
 
-  ngOnInit(): void {
-    this.stockService.getExpensiveStocks().subscribe(stocks => {
-      this.acoes = stocks;
-    });
+   ngOnInit(): void {
+      this.loadAllStocks();
+   }
 
-    this.stockService.getCheapestStocks().subscribe(stocks => {
-      this.acoesBaratas = stocks;
-    });
+   private loadAllStocks(): void {
+      this.expensiveStocksState$ = this.stockService.getExpensiveStocks().pipe(
+         map(stocks => ({ loading: false, stocks: stocks, error: null })),
+         startWith({ loading: true, stocks: null, error: null }),
+         catchError(error => of({ loading: false, stocks: null, error: error })),
+      );
 
-    this.stockService.getFeaturedStocks().subscribe(stocks => {
-      this.acoesDestaque = stocks;
-    });
-  }
+      this.cheapStocksState$ = this.stockService.getCheapestStocks().pipe(
+         map(stocks => ({ loading: false, stocks: stocks, error: null })),
+         startWith({ loading: true, stocks: null, error: null }),
+         catchError(error => of({ loading: false, stocks: null, error: error }))
+      );
+
+      this.featuredStocksState$ = this.stockService.getFeaturedStocks().pipe(
+         map(stocks => ({ loading: false, stocks: stocks, error: null })),
+         startWith({ loading: true, stocks: null, error: null }),
+         catchError(error => of({ loading: false, stocks: null, error: error }))
+      );
+   }
 }
