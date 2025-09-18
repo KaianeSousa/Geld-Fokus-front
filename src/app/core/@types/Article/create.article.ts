f06f28578f8b4c18885f40e3d0eb3c1eda1aa3bb
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-18 00:41:26 -0300

    refactor: Remove slug and authorName from CreateArticle

--- src/app/core/@types/Article/create.article.ts
+++ src/app/core/@types/Article/create.article.ts
@@ -4,8 +4,6 @@ export interface CreateArticle {
    title: string,
    subtitle: string,
    content: string,
-   slug: string,
-   authorName : string,
    articleStatus: ArticleStatus,
    categoryName: string,
    tagNames: string[]
