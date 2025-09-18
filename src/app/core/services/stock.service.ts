1516fb87fca9f1921a8353742350dfea3192359c
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-18 00:40:33 -0300

    feat: Add history to stock data

--- src/app/core/services/stock.service.ts
+++ src/app/core/services/stock.service.ts
@@ -1,44 +1,45 @@
-import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
-import { Stock } from '../@types/Stock';
-import { StockResponse } from '../@types/Stock/StockResponse';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, map} from 'rxjs';
+import {Stock} from '../@types/Stock';
+import {StockResponse} from '../@types/Stock/StockResponse';
 
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class StockService {
-    private readonly endpoint = 'http://localhost:8080/stocks'; 
-  
-    private http = inject(HttpClient);
-  
-    getExpensiveStocks(): Observable<Stock[]> {
+   providedIn: 'root'
+})
+export class StockService {
+   private readonly endpoint = 'http://localhost:8080/stocks';
+
+   private http = inject(HttpClient);
+
+   getExpensiveStocks(): Observable<Stock[]> {
       return this.http.get<StockResponse[]>(`${this.endpoint}/expensive`).pipe(
-        map(stocks => this.transformStocks(stocks))
+         map(stocks => this.transformStocks(stocks))
       );
-    }
-  
-    getCheapestStocks(): Observable<Stock[]> {
+   }
+
+   getCheapestStocks(): Observable<Stock[]> {
       return this.http.get<StockResponse[]>(`${this.endpoint}/cheapest`).pipe(
-        map(stocks => this.transformStocks(stocks))
+         map(stocks => this.transformStocks(stocks))
       );
-    }
-  
-    getFeaturedStocks(): Observable<Stock[]> {
+   }
+
+   getFeaturedStocks(): Observable<Stock[]> {
       return this.http.get<StockResponse[]>(`${this.endpoint}/featured`).pipe(
-        map(stocks => this.transformStocks(stocks))
+         map(stocks => this.transformStocks(stocks))
       );
-    }
-  
-    private transformStocks(stocks: StockResponse[]): Stock[] {
+   }
+
+   private transformStocks(stocks: StockResponse[]): Stock[] {
       return stocks.map(stock => ({
-        symbol: stock.summary.symbol,
-        name: stock.summary.shortName,
-        logoUrl: stock.summary.logoUrl,
-        value: stock.summary.value,
-        change: stock.summary.change.percent,
-        positive: stock.summary.positive
+         symbol: stock.summary.symbol,
+         name: stock.summary.shortName,
+         logoUrl: stock.summary.logoUrl,
+         value: stock.summary.value,
+         change: stock.summary.change.percent,
+         positive: stock.summary.positive,
+         history: stock.history
       }));
-    }
-  }
+   }
+}
