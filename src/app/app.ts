a80906ae92bea736379e23e7e0122e1dd262a94b
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-18 00:37:27 -0300

    feat: Add ToastContainer and new details check

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { Router, RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './shared/components/navbar/navbar.component';
 import {FooterComponent} from './shared/components/footer/footer.component';
 import {StocksSidebar} from './shared/components/stocks-sidebar/stocks-sidebar';
+import {ToastContainer} from './shared/components/toast-container/toast-container';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, StocksSidebar],
+   imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, StocksSidebar, ToastContainer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -17,6 +18,10 @@ export class App {
   private router = inject(Router);
 
   isHome(): boolean {
-    return this.router.url === '/'; 
+    return this.router.url === '/';
+  }
+
+  isNewDetails(): boolean {
+    return this.router.url.startsWith('/news');
   }
 }
