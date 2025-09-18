5500bc0c59425c20bb71fa7f30d551aa371ac53f
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-18 00:40:47 -0300

    refactor: Improve auth interceptor logic

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,51 +1,45 @@
 import {inject} from '@angular/core';
 import {Router} from '@angular/router';
 import {
-  HttpErrorResponse,
-  HttpEvent,
-  HttpHandlerFn,
-  HttpInterceptorFn,
-  HttpRequest,
+   HttpErrorResponse,
+   HttpEvent,
+   HttpHandlerFn,
+   HttpInterceptorFn,
+   HttpRequest,
 } from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 
 export const authInterceptor: HttpInterceptorFn = (
-  request: HttpRequest<unknown>,
-  next: HttpHandlerFn
+   request: HttpRequest<unknown>,
+   next: HttpHandlerFn
 ): Observable<HttpEvent<unknown>> => {
-  const router = inject(Router);
-  const token = localStorage.getItem('token');
-  const baseURL = 'http://localhost:8080';
-  const protectedRoutes = [`${baseURL}/auth/me`, `${baseURL}/wallet`, `${baseURL}/brokerage`];
+   const router = inject(Router);
+   const token = localStorage.getItem('token');
+   const apiBaseUrl = 'http://localhost:8080';
+   const isApiRoute = request.url.startsWith(apiBaseUrl);
+   const isPublicRoute = request.url.includes('/auth/login') || request.url.includes('/auth/register');
 
-  const isProtectedRoute = protectedRoutes.some((url) =>
-    request.url.startsWith(url)
-  );
+   let processedRequest = request;
 
-  let processedRequest = request;
+   if (token && isApiRoute && !isPublicRoute) {
+      processedRequest = request.clone({
+         setHeaders: {
+            Authorization: `Bearer ${token}`,
+         },
+      });
+   }
 
-  if (token && isProtectedRoute) {
-    processedRequest = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
-
-  return next(processedRequest).pipe(
-    catchError((error: unknown) => {
-      if (error instanceof HttpErrorResponse && error.status === 401) {
-        localStorage.removeItem('token');
-
-        alert('Sua sessão expirou. Por favor, faça login novamente.');
-
-        router.navigate(['/login']).then(() => {
-          window.location.reload();
-        });
-      }
-
-      return throwError(() => error);
-    })
-  );
+   return next(processedRequest).pipe(
+      catchError((error: Error) => {
+         if (error instanceof HttpErrorResponse && error.status === 401) {
+            localStorage.removeItem('token');
+            alert('Sua sessão expirou. Por favor, faça login novamente.');
+            router.navigate(['/login']).then(() => {
+               window.location.reload();
+            });
+         }
+         return throwError(() => error);
+      })
+   );
 };
